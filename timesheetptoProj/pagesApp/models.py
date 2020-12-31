a4810e309e0a186ff4d6f0c0274c1bbb13ee7b9b
deeData <deidra108@gmail.com>
2020-12-30 18:16:39 -0700

    first push

---
+++ timesheetptoProj/pagesApp/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+#is not created here
+#no migrations to this app
---
+++ timesheetptoProj/ptoApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PtoappConfig(AppConfig):
+    name = 'ptoApp'
